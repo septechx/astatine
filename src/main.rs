9020690e344e015431a414b503937ed97dfb071e
septech <septechx@proton.me>
2025-04-16 21:55:32 +0200

    Mostly fix icon loading and add default icons

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use freedesktop_desktop_entry::{Iter, default_paths, get_languages_from_env};
+use freedesktop_icons::lookup;
 use fuzzy_matcher::FuzzyMatcher;
 use fuzzy_matcher::skim::SkimMatcherV2;
 use iced::{
-    Size,
-    widget::{column, text, text_input},
+    ContentFit, Size,
+    widget::{column, image, row, svg, text, text_input},
 };
+use icon_loader::IconLoader;
 use std::collections::HashSet;
 
 struct Astatine {
@@ -53,16 +55,43 @@ impl Astatine {
             matched_apps.into_iter().map(|(_, app)| app).collect()
         };
 
+        let icon_loader = IconLoader::new_gtk().unwrap_or(IconLoader::new());
+
         let application_list = filtered_applications
             .iter()
-            .map(|application| -> iced::Element<'_, Message> {
-                text(application.name.clone()).into()
+            .map(|application| {
+                let name = application.name.clone();
+                let icon = application.icon.clone();
+
+                let icon_widget: iced::Element<'_, Message> = if icon.is_empty() {
+                    let icon = icon_loader
+                        .load_icon("application-x-executable")
+                        .unwrap()
+                        .file_for_size(32)
+                        .path()
+                        .to_string_lossy()
+                        .into_owned();
+
+                    svg(icon)
+                        .width(32)
+                        .height(32)
+                        .content_fit(ContentFit::ScaleDown)
+                        .into()
+                } else {
+                    image(icon)
+                        .width(32)
+                        .height(32)
+                        .content_fit(ContentFit::ScaleDown)
+                        .into()
+                };
+
+                row![icon_widget, text(name).center()].spacing(10)
             })
             .fold(column![], |col, element| col.push(element));
 
         column![
             text_input("", &self.search).on_input(Message::SearchChanged),
-            application_list
+            application_list,
         ]
         .into()
     }
@@ -74,10 +103,11 @@ fn main() -> iced::Result {
         .run_with(|| (Astatine::new(), iced::Task::none()))
 }
 
-#[derive(Default, Clone)]
+#[derive(Clone)]
 struct Application {
     name: String,
     exec: String,
+    icon: String,
 }
 
 fn get_applications() -> Vec<Application> {
@@ -95,12 +125,23 @@ fn get_applications() -> Vec<Application> {
             .unwrap_or(std::borrow::Cow::Borrowed(""))
             .to_string();
         let exec = entry.exec().unwrap_or("").to_string();
+        let icon_name = entry.icon().unwrap_or("").to_string();
 
         if name.is_empty() || exec.is_empty() || !seen_execs.insert(exec.clone()) {
             continue;
         }
 
-        applications.push(Application { name, exec });
+        let icon = if !icon_name.is_empty() {
+            lookup(&icon_name)
+                .with_size(32)
+                .find()
+                .map(|p| p.to_string_lossy().into_owned())
+                .unwrap_or_default()
+        } else {
+            String::new()
+        };
+
+        applications.push(Application { name, exec, icon });
     }
 
     applications
